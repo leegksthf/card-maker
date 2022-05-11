55aaaadc836a4543328020cdafba3e15a354e61d
hansol <leecldy@naver.com>
2022-05-11 17:33:23 +0900

    add login and logout functionality

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./app.module.css";
+import Login from "./components/login/login";
+import styles from "./app.module.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Maker from "./components/maker/maker.jsx";
+
+function App({ authService }) {
+  return (
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login authService={authService} />} />
+          <Route path="/maker" element={<Maker authService={authService} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import styles from "./login.module.css";
 
 const Login = ({ authService }) => {
+  const navigate = useNavigate();
+  const goToMaker = (userId) => {
+    navigate("/maker", { state: { id: userId } });
+  };
+
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent) //
-      .then(console.log); // textContent: 모든 요소 가져옴. innerText: 눈에 보이는 텍스트값만 가져옴.
+      .then((data) => goToMaker(data.user.uid)); // textContent: 모든 요소 가져옴. innerText: 눈에 보이는 텍스트값만 가져옴.
   };
+
+  useEffect(() => {
+    authService //
+      .onAuthChange((user) => {
+        user && goToMaker(user.uid);
+      });
+  });
+
   return (
     <section className={styles.login}>
       <Header />
